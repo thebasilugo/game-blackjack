183a4167af726ede010cbf96d43911689bb6e3cf
Basil Ugo <basilugo2@gmail.com>
2024-07-14 17:40:58 +0100

    add dom elements in js

--- blackjack-cpu/dist/app.js
+++ blackjack-cpu/dist/app.js
@@ -1,1 +1,7 @@
 "use strict";
+let messageEl = document.querySelector("#message-el");
+let cardsEl = document.querySelector("#cards-el");
+let sumEl = document.querySelector("#sum-el");
+let startBtn = document.querySelector(".start-btn");
+let newCardBtn = document.querySelector(".newcard-btn");
+let playerEl = document.querySelector("#player-el");
